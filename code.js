45e73abe0e607bb2de7873026d7f22265f467c5b
Khaled AbdRahem <aa.khaled11@gmail.com>
2023-01-01 17:28:20 +0200

    feat: add interactive UI
    add interface displaying choices, result and overall score

--- code.js
+++ code.js
@@ -1,83 +1,85 @@
-// ask player to enter a choice and create a variable to store player's choice
-let playerChoice;
-let gameResult;
-let playerScore = 0;
+let userScore = 0;
 let computerScore = 0;
 
-//create a function getComputerChoice to generate the computer's choice
+const rock_btn = document.querySelector('.rock');
+const paper_btn = document.querySelector('.paper');
+const scissors_btn = document.querySelector('.scissors');
+const userScore_span = document.querySelector('.playerScore');
+const computerScore_span = document.querySelector('.computerScore');
+const roundResult_p = document.querySelector('.roundResult');
 
-function getComputerChoice() {
-    let x =  Math.floor(Math.random() * 3);
-    
-    if (x === 0){
-      return "rock";
-    }
-    else if (x === 1){
-      return "paper";
-    }
-    else if (x === 2){
-      return "scissors";
+function getComputerChoice(){
+    const choice = ['r', 'p', 's']
+    const randomNumber = Math.floor(Math.random()*3);
+    return choice[randomNumber];
+}
+
+function convertToWord(choice){
+    if (choice === 'r') return'Rock';
+    else if (choice === 'p') return'Paper';
+    else if (choice === 's') return'Scissors';
+}
+
+function win(playerChoice,computerChoice){
+        roundResult_p.innerHTML= `You win! ${convertToWord(playerChoice)} beats ${convertToWord(computerChoice)} `;
+        userScore++;
+        userScore_span.innerHTML= userScore;
+        computerScore.innerHTML = computerScore;
+}
+
+function lose(playerChoice,computerChoice){
+    roundResult_p.innerHTML= `You lose! ${convertToWord(playerChoice)} loses to ${convertToWord(computerChoice)} `;
+    computerScore++;
+    userScore_span.innerHTML= userScore;
+    computerScore_span.innerHTML = computerScore;
+}
+function tie(playerChoice,computerChoice){
+    roundResult_p.innerHTML= 'It\'s a tie. try again!' ;
+    userScore_span.innerHTML= userScore;
+    computerScore.innerHTML = computerScore;
+}
+
+
+
+
+
+function game(playerChoice){
+    const computerChoice = getComputerChoice();
+
+    switch(playerChoice +computerChoice){
+    case 'rs' :
+    case 'sp' :
+    case 'pr' :
+        win(playerChoice,computerChoice);
+        break;
+    case 'sr':
+    case 'ps':
+    case 'rp':
+        lose(playerChoice,computerChoice);
+        break;
+
+    case 'rr':
+    case 'pp':
+    case 'ss':
+        tie (playerChoice,computerChoice);
+        break;
     }
 }
 
-let computerChoice = getComputerChoice();
-//create a function to compare both choices and return the winning choice
 
-function playRound(playerChoice, computerChoice){
-    if (playerChoice === "paper" & computerChoice === "rock"){
-        gameResult = "win";
-        playerScore++
-        return"you win! Paper beats rock!";
-    }
-    else if (playerChoice === "scissors" & computerChoice === "rock"){
-        gameResult = "loss";
-        computerScore++
-        return"you lose :( rock beats scissors!"
-    }
-    else if (playerChoice === "rock" & computerChoice === "rock"){
-        return"it's a tie! try again!"
-    }
-    else if (playerChoice === "paper" & computerChoice === "scissors"){
-        gameResult = "loss";
-        computerScore++
-        return"you lose :(  Scissors beats Paper!";
-    }
-    else if (playerChoice === "rock" & computerChoice === "scissors"){
-        gameResult = "win";
-        playerScore++
-        return"you win! Rock beats Scissors!";
-    }
-    else if (playerChoice === "scissors" & computerChoice === "scissors"){
-        return"it's a tie! try again!";
-    }
-    else if (playerChoice === "scissors" & computerChoice === "paper"){
-        gameResult = "win";
-        playerScore++
-        return "you win! Scissors beats Paper!";
-    }
-    else if (playerChoice === "rock" & computerChoice === "paper"){
-        gameResult = "loss";
-        computerScore++
-        return "you lose :( Paper beats Rock!";
-    }
-    else if (playerChoice === "paper" & computerChoice === "paper"){
-        return "it's a tie! try again";
-    }
+
+function main(){
+    rock_btn.addEventListener('click', () =>{
+        game('r');
+    });
+
+    paper_btn.addEventListener('click', () =>{
+        game('p');
+    });
+
+    scissors_btn.addEventListener('click', () =>{
+        game('s');
+    });
 }
 
-// function game(){
-//     for (let i = 0; i<5; i++){
-//         playerInput = prompt("enter rock paper or scissors");
-//         playerChoice = playerInput.toLowerCase();
-//         console.log(playRound(playerChoice, computerChoice));
-//         if (gameResult === "win"){
-//             alert("good job! your score is "+ playerScore + " and the computer's score is "+ computerScore)
-//         }
-//         else if (gameResult === "loss"){
-//             alert("oh no... the computer's score is now " + computerScore + " and yours is still " + playerScore)
-//         }
-//         else {
-//             alert("it's a tie. try again!")
-//         }
-//         }
-// }
+main();
